7b59acbb54a1aa8085f1cf52a6d2c133a01b4994
mwarnsley <marcus.warnsley@gmail.com>
2016-04-13 00:09:28 -0400

    Adding the app.js component and fixing the index html file

---
+++ src/js/components/App.js
@@ -0,0 +1,15 @@
+var React = require("react");
+var AppAction = require("../actions/AppActions");
+var AppStore = require("../stores/AppStore");
+
+var App = React.createClass({
+	render: function(){
+		return (
+			<div>
+				MYAPP
+			</div>
+		)
+	}
+});
+
+module.exports = App;
